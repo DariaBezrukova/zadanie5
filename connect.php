49fd98ec5d3dc803c9b5a99f133b1ee2bd94bc89
DariaBezrukova <90443596+DariaBezrukova@users.noreply.github.com>
2022-05-04 20:51:15 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
-$user = 'u47501';
-$pass = '1469373';
-$db = new PDO('mysql:host=localhost;dbname=u47501', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+$user = 'u47528';
+$pass = '9694962';
+$db = new PDO('mysql:host=localhost;dbname=u47528', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
